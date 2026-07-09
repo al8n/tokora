0011b94f27e370e6d92ab3db4b780f7e479cede1
al8n <scygliu1@gmail.com>
2026-07-09 13:16:43 +1200

    feat(parser): generate the Spread punctuator parsers

--- tokit/src/parser/punct.rs
+++ tokit/src/parser/punct.rs
@@ -163,6 +163,7 @@ define_parsers!(
   FatArrow::fat_arrow::"=>",
   PipeArrow::pipe_arrow::"|>",
   DoubleColon::double_colon::"::",
+  Spread::spread::"...",
   Space::space::" ",
   Tab::tab::"\t",
   Newline::newline::"\n",
@@ -185,3 +186,164 @@ define_parsers!(
   Backtick::backtick::"`",
   Caret::caret::"^",
 );
+
+#[cfg(all(test, feature = "std", feature = "logos"))]
+mod tests {
+  use super::*;
+
+  use crate::{
+    ParserContext,
+    error::token::UnexpectedTokenOf,
+    input::Cursor,
+    lexer::LogosLexer,
+    logos::{self, Logos},
+    span::Spanned,
+    token::Token as TokenTrait,
+  };
+
+  #[derive(Debug, Clone, Logos, PartialEq)]
+  #[logos(crate = logos, skip r"[ \t\r\n]+")]
+  enum Token {
+    #[token("...")]
+    Spread,
+    #[regex(r"[0-9]+")]
+    Num,
+  }
+
+  #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+  enum TokenKind {
+    Spread,
+    Num,
+  }
+
+  impl core::fmt::Display for TokenKind {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+      match self {
+        TokenKind::Spread => write!(f, "..."),
+        TokenKind::Num => write!(f, "number"),
+      }
+    }
+  }
+
+  impl TokenTrait<'_> for Token {
+    type Kind = TokenKind;
+    type Error = ();
+
+    fn kind(&self) -> TokenKind {
+      match self {
+        Token::Spread => TokenKind::Spread,
+        Token::Num => TokenKind::Num,
+      }
+    }
+
+    fn is_trivia(&self) -> bool {
+      false
+    }
+  }
+
+  impl PunctuatorToken<'_> for Token {
+    fn spread() -> Option<Self::Kind> {
+      Some(TokenKind::Spread)
+    }
+  }
+
+  type TestLexer<'a> = LogosLexer<'a, Token>;
+
+  #[derive(Debug)]
+  struct E;
+
+  impl From<()> for E {
+    fn from(_: ()) -> Self {
+      E
+    }
+  }
+
+  impl<'a, T, Kind: Clone, S, Lang: ?Sized> From<UnexpectedToken<'a, T, Kind, S, Lang>> for E {
+    fn from(_: UnexpectedToken<'a, T, Kind, S, Lang>) -> Self {
+      E
+    }
+  }
+
+  impl From<UnexpectedEot> for E {
+    fn from(_: UnexpectedEot) -> Self {
+      E
+    }
+  }
+
+  struct TestEm;
+
+  impl<'inp> Emitter<'inp, TestLexer<'inp>> for TestEm {
+    type Error = E;
+
+    fn emit_lexer_error(
+      &mut self,
+      _: Spanned<
+        <<TestLexer<'inp> as Lexer<'inp>>::Token as TokenTrait<'inp>>::Error,
+        <TestLexer<'inp> as Lexer<'inp>>::Span,
+      >,
+    ) -> Result<(), E>
+    where
+      TestLexer<'inp>: Lexer<'inp>,
+    {
+      Err(E)
+    }
+
+    fn emit_unexpected_token(
+      &mut self,
+      _: UnexpectedTokenOf<'inp, TestLexer<'inp>>,
+    ) -> Result<(), E>
+    where
+      TestLexer<'inp>: Lexer<'inp>,
+    {
+      Err(E)
+    }
+
+    fn emit_error(
+      &mut self,
+      err: Spanned<E, <TestLexer<'inp> as Lexer<'inp>>::Span>,
+    ) -> Result<(), E>
+    where
+      TestLexer<'inp>: Lexer<'inp>,
+    {
+      Err(err.into_data())
+    }
+
+    fn rewind(&mut self, _: &Cursor<'inp, '_, TestLexer<'inp>>)
+    where
+      TestLexer<'inp>: Lexer<'inp>,
+    {
+    }
+  }
+
+  fn ctx() -> ParserContext<'static, TestLexer<'static>, TestEm> {
+    ParserContext::new(TestEm)
+  }
+
+  #[test]
+  fn spread_try_parse_accepts_spread_token() {
+    fn parse<'inp>(
+      inp: &mut InputRef<'inp, '_, TestLexer<'inp>, ParserContext<'inp, TestLexer<'inp>, TestEm>>,
+    ) -> Result<bool, E> {
+      Ok(Spread::try_parse(inp)?.is_accept())
+    }
+    let r: Result<bool, _> = Parser::with_context(ctx()).apply(parse).parse_str("...");
+    assert!(r.unwrap());
+  }
+
+  #[test]
+  fn spread_try_parse_declines_non_spread_token() {
+    fn parse<'inp>(
+      inp: &mut InputRef<'inp, '_, TestLexer<'inp>, ParserContext<'inp, TestLexer<'inp>, TestEm>>,
+    ) -> Result<(bool, bool), E> {
+      let declined = Spread::try_parse(inp)?.is_decline();
+      let next_is_num = inp
+        .try_expect(|t| t.data.kind() == TokenKind::Num)?
+        .is_some();
+      Ok((declined, next_is_num))
+    }
+    let r: Result<(bool, bool), _> = Parser::with_context(ctx()).apply(parse).parse_str("42");
+    let (declined, next_is_num) = r.unwrap();
+    assert!(declined);
+    assert!(next_is_num);
+  }
+}
